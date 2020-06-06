e3e98160ef4596a99d105c69bf3b3d7c55ef0922
Shawn <codingshawn@gmail.com>
2020-06-06 21:15:26 +0800

    added basic calculators function. Also prevented bugs that occur when trying to continue/start new calculation after pressing equals. Also allowed multiple operations to occur without pressing equals

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+const calcScreen = document.getElementById('calcScreen');
+const nums = Array.from(document.querySelectorAll('.nums'));
+const operators = Array.from(document.querySelectorAll('.operators'));
+const equals = document.getElementById('equals');
+const clear = document.getElementById('clear');
+let newCalculation = true; //to reset calculator if press num after equals
+let memory;
+let operator;
+
+nums.forEach(num => num.addEventListener('click', updateScreenNum));
+
+operators.forEach(operator => operator.addEventListener('click', selectOperator));
+
+equals.addEventListener('click', calculate);
+
+clear.addEventListener('click', clearScreen);
+
+function updateScreenNum() {
+    if (newCalculation == false) {
+        clearScreen();
+    }
+    if (calcScreen.textContent.trim().length <= 9) {
+        Number(calcScreen.textContent) == 0 ? calcScreen.textContent = this.textContent: 
+                        calcScreen.textContent += this.textContent;
+    }
+}
+
+function selectOperator() {
+    if (!operator == "") {
+        calculate();
+        console.log(`answer: ${memory}`)
+    }
+    newCalculation = true;
+    memory = Number(calcScreen.textContent);
+    console.log(memory);
+    operator = this.textContent.trim();
+    calcScreen.textContent = "0";
+}
+
+function calculate() {
+    let newNum = Number(calcScreen.textContent);
+    let answer;
+    console.log(operator);
+    if (operator == "+") {
+        answer = add(memory, newNum);
+    } else if (operator == "-") {
+        answer = subtract(memory, newNum);
+    } else if (operator == "*") {
+        answer = multiply(memory, newNum);
+    } else if (operator == "/") {
+        answer = divide(memory, newNum);
+        answer = Math.round(answer * 1000000000) / 1000000000; //keep decimal place to 9 decimal place
+    }
+    calcScreen.textContent = answer;
+    memory = answer;
+    operator = "";
+    newCalculation = false;
+}
+
+function clearScreen() {
+    memory = 0;
+    operator = "";
+    calcScreen.textContent = memory
+    newCalculation = true;
+}
+
 function add(a, b) {
     return a + b;
 }
