04f34bf227d8b3ecf2786771257473cedc7cd9a2
Shawn <codingshawn@gmail.com>
2020-06-06 21:31:29 +0800

    Prevent bugs when pressing equals consecutively

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const operators = Array.from(document.querySelectorAll('.operators'));
 const equals = document.getElementById('equals');
 const clear = document.getElementById('clear');
 let newCalculation = true; //to reset calculator if press num after equals
+let preventDoubleOperator = true; // allow switching of operators during calculation
+let preventDoubleEquals = true; // prevent errors when pressing equals consecutively
 let memory;
 let operator;
 
@@ -23,38 +25,48 @@ function updateScreenNum() {
         Number(calcScreen.textContent) == 0 ? calcScreen.textContent = this.textContent: 
                         calcScreen.textContent += this.textContent;
     }
+    preventDoubleOperator = true;
+    preventDoubleEquals = true;
 }
 
 function selectOperator() {
-    if (!operator == "") {
-        calculate();
-        console.log(`answer: ${memory}`)
-    }
-    newCalculation = true;
-    memory = Number(calcScreen.textContent);
-    console.log(memory);
-    operator = this.textContent.trim();
-    calcScreen.textContent = "0";
+    if (preventDoubleOperator == true) {
+        if (!operator == "") {
+            calculate();
+            console.log(`answer: ${memory}`)
+        }
+        preventDoubleEquals = true;
+        newCalculation = true;
+        memory = Number(calcScreen.textContent);
+        console.log(memory);
+        operator = this.textContent.trim();
+        calcScreen.textContent = "0";
+        preventDoubleOperator = false;
+    } else operator = this.textContent.trim();
 }
 
 function calculate() {
-    let newNum = Number(calcScreen.textContent);
-    let answer;
-    console.log(operator);
-    if (operator == "+") {
-        answer = add(memory, newNum);
-    } else if (operator == "-") {
-        answer = subtract(memory, newNum);
-    } else if (operator == "*") {
-        answer = multiply(memory, newNum);
-    } else if (operator == "/") {
-        answer = divide(memory, newNum);
-        answer = Math.round(answer * 1000000000) / 1000000000; //keep decimal place to 9 decimal place
+    if (preventDoubleEquals == true) {
+        let newNum = Number(calcScreen.textContent);
+        let answer;
+        console.log(operator);
+        if (operator == "+") {
+            answer = add(memory, newNum);
+        } else if (operator == "-") {
+            answer = subtract(memory, newNum);
+        } else if (operator == "*") {
+            answer = multiply(memory, newNum);
+        } else if (operator == "/") {
+            answer = divide(memory, newNum);
+            answer = Math.round(answer * 1000000000) / 1000000000; //keep decimal place to 9 decimal place
+        }
+        calcScreen.textContent = answer;
+        memory = answer;
+        operator = "";
+        newCalculation = false;
+        preventDoubleOperator = true;
+        preventDoubleEquals = false;
     }
-    calcScreen.textContent = answer;
-    memory = answer;
-    operator = "";
-    newCalculation = false;
 }
 
 function clearScreen() {
@@ -62,6 +74,8 @@ function clearScreen() {
     operator = "";
     calcScreen.textContent = memory
     newCalculation = true;
+    preventDoubleOperator = true;
+    preventDoubleEquals = true;
 }
 
 function add(a, b) {
